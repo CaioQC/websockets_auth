f028862c71de5e2e4ac82e088818c04d734a1b2d
CaioQC <caioq2004@gmail.com>
2025-07-22 15:07:39 -0300

    Cadastrando e salvando usuários no banco de dados e criptografando a senha

--- public/cadastro/cadastro.js
+++ public/cadastro/cadastro.js
@@ -1,10 +1,12 @@
+import { emitirCadastrarUsuario } from "./socket_front_cadastro.js"
+
 const form = document.getElementById("form-cadastro")
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    const usuario = form["input-usuario"].value
+    const nome = form["input-usuario"].value
     const senha = form["input-senha"].value
 
-    console.log(usuario, senha)
+    emitirCadastrarUsuario({ nome, senha })
 })
--- servidor/db/dbConnect.js
+++ servidor/db/dbConnect.js
@@ -1,21 +1,25 @@
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
 
 dotenv.config()
 
 const cliente = new MongoClient(process.env.STRING_CONEXAO);
 
 let documentosColecao;
+let usuariosColecao
 
 try {
   await cliente.connect();
 
-  const db = cliente.db("alura-websockets");
-  documentosColecao = db.collection("documentos");
+  const db = cliente.db("websockets");
+  documentosColecao = db.collection("documents");
+  usuariosColecao = db.collection("usuarios")
 
   console.log("Conectado ao banco de dados com sucesso!");
-} catch (erro) {
+}
+
+catch (erro) {
   console.log(erro);
 }
 
-export { documentosColecao };
+export { documentosColecao, usuariosColecao };
